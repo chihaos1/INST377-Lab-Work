cc1d1b49419599081fd74d11b43b3b1ccaa5209f
Chi-Hao Sheng <chihaos0629@gmail.com>
2021-09-23 18:34:21 -0400

    Updated lab 4

---
+++ client/lab_4/script.js
@@ -0,0 +1,41 @@
+let slidePosition = 0;
+const slides = document.querySelectorAll('.carousel-item');
+const totalSlides = slides.length;
+
+document.
+    querySelector('.next')
+    .addEventListener('click', function() {
+        moveToNextSlide();
+    })
+
+document.
+    querySelector('.prev')
+    .addEventListener('click', function() {
+        moveToPrevSlide();
+    })
+
+function updateSlidePosition() {
+    for (let slide of slides) {
+        slide.classList.remove('carousel-item--visible');
+        slide.classList.add('carousel-item');
+    }
+    slides[slidePosition].classList.add('carousel-item--visible')
+}
+
+function moveToNextSlide() {
+    if (slidePosition === totalSlides - 1) {
+        slidePosition = 0;
+    } else {
+        slidePosition++;
+    }
+    updateSlidePosition()
+}
+
+function moveToPrevSlide() {
+    if (slidePosition === 0) {
+        slidePosition = totalSlides - 1;
+    } else {
+        slidePosition--;
+    }
+    updateSlidePosition()
+}
